3fcad64ca1083dd8b02469902b3f1c597678684d
yusuke1403 <69718302+yusuke1403@users.noreply.github.com>
2020-10-07 12:33:51 +0900

    seed_fu

---
+++ db/fixtures/02_image.rb
@@ -0,0 +1,80 @@
+Image.seed do |s|
+  s.id = 1
+  s.src = "shop_01_01.jpg"
+  s.shop_id = 1
+end
+Image.seed do |s|
+  s.id = 2
+  s.src = "shop_01_02.jpg"
+  s.shop_id = 1
+end
+Image.seed do |s|
+  s.id = 3
+  s.src = "shop_02_01.jpg"
+  s.shop_id = 2
+end
+Image.seed do |s|
+  s.id = 4
+  s.src = "shop_02_02.jpg"
+  s.shop_id = 2
+end
+Image.seed do |s|
+  s.id = 5
+  s.src = "shop_03_01.jpg"
+  s.shop_id = 3
+end
+Image.seed do |s|
+  s.id = 6
+  s.src = "shop_03_02.jpg"
+  s.shop_id = 3
+end
+Image.seed do |s|
+  s.id = 7
+  s.src = "shop_04_01.jpg"
+  s.shop_id = 4
+end
+Image.seed do |s|
+  s.id = 8
+  s.src = "shop_04_02.jpg"
+  s.shop_id = 4
+end
+Image.seed do |s|
+  s.id = 9
+  s.src = "shop_05_01.jpg"
+  s.shop_id = 5
+end
+Image.seed do |s|
+  s.id = 10
+  s.src = "shop_05_02.jpg"
+  s.shop_id = 5
+end
+Image.seed do |s|
+  s.id = 11
+  s.src = "shop_06_01.jpg"
+  s.shop_id = 6
+end
+Image.seed do |s|
+  s.id = 12
+  s.src = "shop_07_01.jpg"
+  s.shop_id = 7
+end
+Image.seed do |s|
+  s.id = 13
+  s.src = "shop_07_02.jpg"
+  s.shop_id = 7
+end
+Image.seed do |s|
+  s.id = 14
+  s.src = "shop_08_01.jpg"
+  s.shop_id = 8
+end
+Image.seed do |s|
+  s.id = 15
+  s.src = "shop_08_02.jpg"
+  s.shop_id = 8
+end
+Image.seed do |s|
+  s.id = 16
+  s.src = "shop_09_01.jpg"
+  s.shop_id = 9
+end
